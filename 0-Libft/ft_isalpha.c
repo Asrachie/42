412faeb065d24e7eb8b87cc0685c200dc0e3a26d
Asrachie <107409423+Asrachie@users.noreply.github.com>
2024-03-13 00:20:43 +0100

    :bulb: remove comments

--- 0-Libft/ft_isalpha.c
+++ 0-Libft/ft_isalpha.c
@@ -4,16 +4,3 @@ int	ft_isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
-
-//#include <stdio.h>
-//int main(void)
-//{
-//	int	i;
-//	i = 0;
-//	while (i <= 127)
-//	{
-//		printf("%d   %d\n", i, ft_isalpha(i));
-//		i++;
-//	}
-//	return (0);
-//}
--- 0-Libft/ft_strncmp.c
+++ 0-Libft/ft_strncmp.c
@@ -43,11 +43,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (v_s1[i] && v_s2[i] && v_s1[i] == v_s2[i] && n > 0)
-	{
+	while (i < n && v_s1[i] && v_s2[i] && v_s1[i] == v_s2[i])
 		i++;
-		n--;
-	}
 	return (v_s1[i] - v_s2[i]);
 }
 
