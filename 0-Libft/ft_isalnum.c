31f9fb107438ff0f8808e15265894a4a57aff7a0
Asrachie <107409423+Asrachie@users.noreply.github.com>
2024-03-13 00:28:12 +0100

    :bulb: remove comments

--- 0-Libft/ft_isalnum.c
+++ 0-Libft/ft_isalnum.c
@@ -4,15 +4,3 @@ int	ft_isalnum(int c)
 {
 	return (ft_isalpha(c) || ft_isdigit(c));
 }
-
-//int main(void)
-//{
-//	int	i;
-//	i = 0;
-//	while (i <= 127)
-//	{
-//		printf("%d   %d\n", i, ft_isalnum(i));
-//		i++;
-//	}
-//	return (0);
-//}
