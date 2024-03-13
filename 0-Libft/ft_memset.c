ba9f40d27a5134b85482a56c7648031bf8401c62
Asrachie <107409423+Asrachie@users.noreply.github.com>
2024-03-13 01:36:56 +0100

    :bulb: :recycle: remove comments, exception handling

--- 0-Libft/ft_memset.c
+++ 0-Libft/ft_memset.c
@@ -1,18 +1,3 @@
-//NAME
-//       memset - fill memory with a constant byte
-//
-//SYNOPSIS
-//       #include <string.h>
-//
-//       void *memset(void *s, int c, size_t n);
-//
-//DESCRIPTION
-//       The  memset() function fills the first n bytes of the memory area
-//       pointed to by s with the constant byte c.
-//
-//RETURN VALUE
-//       The memset() function returns a pointer to the memory area s.
-
 #include "libft.h"
 
 void	*ft_memset(void *s, int c, size_t n)
@@ -20,6 +5,8 @@ void	*ft_memset(void *s, int c, size_t n)
 	unsigned char	*v_s;
 	size_t			i;
 
+	if (!s || n == 0)
+		return (s);
 	v_s = (unsigned char *)s;
 	i = 0;
 	while (i < n)
@@ -29,10 +16,3 @@ void	*ft_memset(void *s, int c, size_t n)
 	}
 	return (s);
 }
-
-//int main(void)
-//{
-//	char a[10];
-//	printf("%s\n", (char *)ft_memset(a, '1', 5));
-//	return (0);
-//}
