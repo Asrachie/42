a650e4735855ece5d20a9ca23405fc6c4e92347e
Asrachie <107409423+Asrachie@users.noreply.github.com>
2024-03-13 12:37:23 +0100

    :bug: :recycle: exception handling(NULL)

--- 0-Libft/ft_strlcpy.c
+++ 0-Libft/ft_strlcpy.c
@@ -2,14 +2,19 @@
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
-	size_t	i;
+	size_t	src_len;
 
-	i = 0;
-	while (src[i] && i + 1 < size)
+	if (!dst || !src)
+		return (0);
+	src_len = ft_strlen(src);
+	if (size == 0)
+		return (src_len);
+	if (src_len < size - 1)
+		ft_memmove(dst, src, src_len + 1);
+	else
 	{
-		dst[i] = src[i];
-		i++;
+		ft_memmove(dst, src, size - 1);
+		dst[size - 1] = '\0';
 	}
-	dst[i] = '\0';
-	return (i);
+	return (src_len);
 }
