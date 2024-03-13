34338255b8174f3ba6c83cf3360ab2463004b47d
Asrachie <107409423+Asrachie@users.noreply.github.com>
2024-03-13 02:16:17 +0100

    :bulb: :recycle: remove comments, exception handling(NULL, if (dest == src))

--- 0-Libft/ft_memmove.c
+++ 0-Libft/ft_memmove.c
@@ -1,21 +1,3 @@
-//NAME
-//       memmove - copy memory area
-//
-//SYNOPSIS
-//       #include <string.h>
-//
-//       void *memmove(void *dest, const void *src, size_t n);
-//
-//DESCRIPTION
-//       The  memmove() function copies n bytes from memory area src to mem
-//       ory area dest.  The memory areas may overlap: copying  takes  place
-//       as  though the bytes in src are first copied into a temporary array
-//       that does not overlap src or dest, and the bytes  are  then  copied
-//       from the temporary array to dest.
-//
-//RETURN VALUE
-//       The memmove() function returns a pointer to dest.
-
 #include "libft.h"
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
@@ -23,6 +5,10 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char		*v_dest;
 	const unsigned char	*v_src;
 
+	if (!dest || !src)
+		return (NULL);
+	if (dest == src)
+		return (dest);
 	v_dest = (unsigned char *)dest;
 	v_src = (const unsigned char *)src;
 	if (v_dest < v_src)
@@ -34,13 +20,3 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	}
 	return (dest);
 }
-
-//int main() {
-//	char des[10];
-//	char str[10] = "1234567890";
-//
-//	ft_memmove(des, str, 5);
-//	printf("%s\n", des);
-//
-//	return 0;
-//}
