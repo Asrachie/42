0b9c1a9089ed022a2bf70bbd46a9f3aa15ef12da
Asrachie <107409423+Asrachie@users.noreply.github.com>
2024-03-13 01:20:04 +0100

    :recycle: exception handling(NULL)

--- 0-Libft/ft_strlen.c
+++ 0-Libft/ft_strlen.c
@@ -1,24 +1,11 @@
-//NAME
-//       strlen - calculate the length of a string
-//
-//SYNOPSIS
-//       #include <string.h>
-//
-//       size_t strlen(const char *s);
-//
-//DESCRIPTION
-//       The  strlen()  function calculates the length of the string pointed
-//       to by s, excluding the terminating null byte ('\0').
-//
-//RETURN VALUE
-//       The strlen() function returns the number of  bytes  in  the  string
-//       pointed to by s.
 #include "libft.h"
 
 size_t	ft_strlen(const char *str)
 {
 	size_t	i;
 
+	if (!str)
+		return (0);
 	i = 0;
 	while (str[i])
 		i++;
