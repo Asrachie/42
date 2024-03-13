55d356695d0c40843739f6f5c439c002c5330b43
Asrachie <107409423+Asrachie@users.noreply.github.com>
2024-03-13 22:10:53 +0100

    :bug: fix exception handling(remove size == 0)

--- 0-Libft/ft_strlcat.c
+++ 0-Libft/ft_strlcat.c
@@ -6,7 +6,7 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	dst_len;
 	size_t	src_len;
 
-	if (!dst || !src || size == 0)
+	if (!dst || !src)
 		return (0);
 	dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
