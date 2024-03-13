576e6cc71d6edfc3a7f71df473b8de2e718e7886
Asrachie <107409423+Asrachie@users.noreply.github.com>
2024-03-13 22:55:30 +0100

    :recycle: modify variable name

--- 0-Libft/ft_strtrim.c
+++ 0-Libft/ft_strtrim.c
@@ -2,16 +2,16 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	index;
-	size_t	last_index;
+	size_t	start_index;
+	size_t	end_index;
 
 	if (!s1 || !set)
 		return (NULL);
-	index = 0;
-	last_index = ft_strlen(s1);
-	while (ft_strchr(set, s1[index]) && s1[index])
-		index++;
-	while (ft_strchr(set, s1[last_index - 1]) && last_index > index)
-		last_index--;
-	return (ft_substr(s1, index, last_index - index));
+	start_index = 0;
+	end_index = ft_strlen(s1);
+	while (ft_strchr(set, s1[start_index]) && s1[start_index])
+		start_index++;
+	while (ft_strchr(set, s1[end_index - 1]) && end_index > start_index)
+		end_index--;
+	return (ft_substr(s1, start_index, end_index - start_index));
 }
